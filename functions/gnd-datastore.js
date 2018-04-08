7312e6581414ab4ee54e760132ddccad7fc51a0b
Gino Miceli <gmiceli@google.com>
2018-04-08 17:06:08 -0400

    Finish impl of per-project Sheets sync and refactored/cleaned up related code

--- functions/gnd-datastore.js
+++ functions/gnd-datastore.js
@@ -9,16 +9,24 @@ class GndDatastore {
     return docRef.get().then(doc => doc.exists ? doc.data() : null);
   }
 
+  fetchDoc_(path) {
+    return this.fetch_(this.db_.doc(path));    
+  }
+
   fetchRecord(projectId, featureId, recordId) {
-    return this.fetch_(this.db_.doc(`projects/${projectId}/features/${featureId}/records/${recordId}`));
+    return this.fetchDoc_(`projects/${projectId}/features/${featureId}/records/${recordId}`);
   }
 
   fetchForm(projectId, featureTypeId, formId) {
-    return this.fetch_(this.db_.doc(`projects/${projectId}/featureTypes/${featureTypeId}/forms/${formId}`));
+    return this.fetchDoc_(`projects/${projectId}/featureTypes/${featureTypeId}/forms/${formId}`);
   }
 
   fetchFeature(projectId, featureId) {
-    return this.fetch_(this.db_.doc(`projects/${projectId}/features/${featureId}`));
+    return this.fetchDoc_(`projects/${projectId}/features/${featureId}`);
+  }
+
+  fetchSheetsConfig(projectId) {
+    return this.fetchDoc_(`projects/${projectId}/sheets/config`);
   }
 }
 
--- functions/index.js
+++ functions/index.js
@@ -2,95 +2,35 @@
 
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const GndSpreadsheet = require('./gnd-spreadsheet');
+const util = require('util');
 const GndAuth = require('./gnd-auth');
+const GndCloudFunctions = require('./gnd-cloud-functions')
 const GndDatastore = require('./gnd-datastore')
 
 // functions.config().firebase is auto-populated with configuration needed to
 // initialize the firebase-admin SDK when deploying via Firebase CLI.
 admin.initializeApp(functions.config().firebase);
 
-// TODO: Use firebase functions:config:set to configure your googleapi object:
-// googleapi.sheet_id = Google Sheet id (long string in middle of sheet URL)
-const CONFIG_SHEET_ID = '12qqPuHs7UYN0HlwfxDl3HtO-8ushkGOqs8_5p6lPwAM';
-
 const db = new GndDatastore(admin.firestore());
 const auth = new GndAuth();
+const gnd = new GndCloudFunctions(db, auth);
 
-// TODO: Make dynamic
-const sheet = new GndSpreadsheet(auth, CONFIG_SHEET_ID);
-
-// Test:
+// Test via shell:
 // updateColumns.get('/?project=R06MucQJSWvERdE7SiL1&featureType=aaaaaaaa&form=1234567')
-exports.updateColumns = functions.https.onRequest((req, res) => {
-  const {
-    project: projectId,
-    featureType: featureTypeId,
-    form: formId
-  } = req.query;
-  return sheet.getColumnsByElementId().then(colResp => {
-    if (!colResp) {
-      res.status(500).send('Could not fetch existing sheet columns');
-      return;
-    }
-    const mds = colResp.matchedDeveloperMetadata || [];
-    // TODO: Use actual max startIndex, not len.
-    const insertAt = mds.length == 0 ? 0 : 
-      mds[mds.length-1].developerMetadata.location.dimensionRange.endIndex;
-    const existingColumns = mds.map(md => md.developerMetadata.metadataValue);
-    return db.fetchForm(projectId, featureTypeId, formId).then(form => {
-      if (!form) {
-        res.status(400).send('Form definition not found');
-        return;
-      }
-      if (!form.elements || form.elements.length == 0) {
-        res.status(500).send('Form definition contains no elements');
-        return;
-      }
-      // TODO: Update existing column headings when label changes.
-      return sheet
-        .updateColumns(existingColumns, form.elements, insertAt)
-        .then(cnt => res.send(`${cnt} columns added`));
-    });
-  });
-});
+exports.updateColumns = functions.https.onRequest((req, res) => 
+  gnd.updateSpreadsheetColumns(req, res)
+    .catch(err => res.status(500).send(`${err}`)));
 
-// Test:
+// Test via shell:
 // onCreateRecord({featureTypeId: 'households', formId: '1', responses: {'interviewer': 'Nikola Tesla'}}, {params: {projectId: 'R06MucQJSWvERdE7SiL1', featureId: 'p9lyePfXYPOByUFpnIVp', recordId: 'newRecord'}});
 exports.onCreateRecord = functions.firestore
   .document('projects/{projectId}/features/{featureId}/records/{recordId}')
-  .onCreate((change, context) => {
-    const {projectId, featureId, recordId} = context.params;
-    const record = change.data();
-    const {featureTypeId, formId} = record;
-    return sheet.getColumnIds().then(colIds => {
-        if (colIds.length == 0) {
-          console.log('No columns to update');
-          return;
-        }
-        return db.fetchFeature(projectId, featureId).then(feature => {
-          if (!feature) {
-            console.log('Feature not found: ', featureId, ' Project: ', projectId);
-            return;
-          }
-          return sheet.addRow(feature, recordId, record, colIds);
-        });
-      });
-    });
+  .onCreate((change, context) => 
+     gnd.onCreateRecord(change, context));
 
+// Test via shell:
 // onUpdateRecord({after: {featureTypeId: 'households', formId: '1', responses: {'interviewer': 'George Washington'}}}, {params: {projectId: 'R06MucQJSWvERdE7SiL1', featureId: 'p9lyePfXYPOByUFpnIVp', recordId: 'newRecord'}});
 exports.onUpdateRecord = functions.firestore
   .document('projects/{projectId}/features/{featureId}/records/{recordId}')
-  .onUpdate((change, context) => {
-    const {projectId, featureId, recordId} = context.params;
-    const record = change.after.data();
-    const {featureTypeId, formId} = record;
-    return sheet.getColumnIds().then(colIds => {
-        if (colIds.length == 0) {
-          console.log('No columns to update');
-          return {};
-        }
-        return db.fetchFeature(projectId, featureId)
-          .then(feature => sheet.updateRow(feature, recordId, record, colIds));
-      }).then(_ => 'OK');
-    });
+  .onUpdate((change, context) => 
+     gnd.onUpdateRecord(change, context));
