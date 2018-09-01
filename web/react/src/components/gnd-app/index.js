eb1366c47d6c0d3acf884993a0ddecfdb28f34d6
Gino Miceli <gmiceli@google.com>
2018-09-01 18:58:44 -0400

    Add map, licenses

--- web/react/src/components/gnd-app/index.js
+++ web/react/src/components/gnd-app/index.js
@@ -1,4 +1,22 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 import React from 'react';
+import GndMap from '../gnd-map';
 import GndProjectEditor from '../gnd-project-editor';
 import { connect } from 'react-redux'
 import { compose } from 'redux'
@@ -59,7 +77,7 @@ class GndApp extends React.Component {
             className="edit-project"
             onClick={this.handleEditProjectClick}>Edit project</Button>
           <Button 
-            variant="outlined"
+            variant="contained"
             size="small" 
             color="primary"
             className="sign-out"
@@ -67,6 +85,7 @@ class GndApp extends React.Component {
          </React.Fragment> );
     return (
       <React.Fragment>
+        <GndMap></GndMap>
         <div className="top-right-controls">
           {AuthWiget}  
         </div>
