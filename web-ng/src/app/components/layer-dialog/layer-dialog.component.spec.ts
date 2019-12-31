f52bca7101de051f422eac9bdcaa36af4314ca30
Gino Miceli <gmiceli@google.com>
2019-12-30 23:50:33 -0500

    Mock LayerDialogComponent test deps

--- web-ng/src/app/components/layer-dialog/layer-dialog.component.spec.ts
+++ web-ng/src/app/components/layer-dialog/layer-dialog.component.spec.ts
@@ -15,16 +15,28 @@
  */
 
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component } from '@angular/core';
 import { LayerDialogComponent } from './layer-dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({ selector: 'mat-dialog-content', template: '' })
+class MatDialogContent {}
+
+@Component({ selector: 'mat-dialog-actions', template: '' })
+class MatDialogActions {}
 
 describe('LayerDialogComponent', () => {
   let component: LayerDialogComponent;
   let fixture: ComponentFixture<LayerDialogComponent>;
+  let dialogRef: Partial<MatDialogRef<LayerDialogComponent>> = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LayerDialogComponent],
+      declarations: [LayerDialogComponent, MatDialogContent, MatDialogActions],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: dialogRef },
+      ],
     }).compileComponents();
   }));
 
