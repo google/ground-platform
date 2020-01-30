64a67a69e4475a56628c1c67d004d61ec87d107d
Gino Miceli <gmiceli@google.com>
2020-01-30 18:17:14 -0500

    Streaming features and observations

--- web-ng/src/app/components/feature-panel/feature-panel.component.ts
+++ web-ng/src/app/components/feature-panel/feature-panel.component.ts
@@ -1,15 +1,41 @@
+// TODO: Copyright headers!
+
+import { Observation } from './../../shared/models/observation/observation.model';
+import { ObservationService } from './../../services/observation/observation.service';
+import { FeatureService } from './../../services/feature/feature.service';
+import { switchMap } from 'rxjs/operators';
+import { ProjectService } from './../../services/project/project.service';
+import { List } from 'immutable';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-feature-panel',
   templateUrl: './feature-panel.component.html',
-  styleUrls: ['./feature-panel.component.css']
+  styleUrls: ['./feature-panel.component.css'],
 })
 export class FeaturePanelComponent implements OnInit {
+  private observations$: Observable<List<Observation>>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private projectService: ProjectService,
+    private featureService: FeatureService,
+    private observationService: ObservationService
+  ) {
+    this.observations$ = projectService
+      .getActiveProject$()
+      .pipe(
+        switchMap(project =>
+          featureService
+            .getActiveFeature$()
+            .pipe(
+              switchMap(feature =>
+                observationService.observations$(project, feature)
+              )
+            )
+        )
+      );
   }
 
+  ngOnInit() {}
 }
