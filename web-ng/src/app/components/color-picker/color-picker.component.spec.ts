4af6cef50dd602eeb053421a29da95369ebfbd5b
parulraheja98 <parulraheja98@gmail.com>
2020-01-31 13:55:03 -0600

    Added Copyright header

--- web-ng/src/app/components/color-picker/color-picker.component.spec.ts
+++ web-ng/src/app/components/color-picker/color-picker.component.spec.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ColorPickerComponent } from './color-picker.component';
 import { CommonModule } from '@angular/common';
@@ -12,7 +28,7 @@ describe('ColorPickerComponent', () => {
   let component: ColorPickerComponent;
   let fixture: ComponentFixture<ColorPickerComponent>;
   const dialogRef: Partial<MatDialogRef<ColorPickerComponent>> = {};
-  
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ColorPickerComponent],
