3c3603e8dee597047a5d910c7314b8fe0224d2eb
Maral Ziobro <maralka@google.com>
2020-04-22 11:38:00 -0400

    extract Response objects from angular form

--- web-ng/src/app/components/observation-form/observation-form.component.ts
+++ web-ng/src/app/components/observation-form/observation-form.component.ts
@@ -22,6 +22,7 @@ export class ObservationFormComponent {
   readonly lang: string;
   readonly fieldTypes = FieldType;
   readonly cardinality = Cardinality;
+  observation?: Observation;
   observationForm?: FormGroup;
   observationFields?: List<Field>;
   payLoad = '';
@@ -35,22 +36,123 @@ export class ObservationFormComponent {
     observationService
       .getSelectedObservation$()
       .subscribe((observation?: Observation) => {
-        this.initForm(observation);
+        this.observation = observation;
         this.observationFields = observation!
           .form!.fields.toOrderedMap()
           .sortBy((k, v) => k.label)
           .toList();
+        this.initForm();
       });
   }
 
-  initForm(observation?: Observation) {
-    if (observation === undefined) {
+  initForm() {
+    if (this.observation === undefined) {
       throw Error('Observation is not selected.');
     }
-    this.observationForm = this.getFormGroupForObservation(observation);
+    this.observationForm = this.convertObservationToFormGroup(
+      this.observation!
+    );
   }
 
-  populateFormGroupWithControlsForSelectOneField(
+  onSave() {
+    this.payLoad = JSON.stringify(this.observationForm?.getRawValue());
+    console.log(this.extractResponses());
+  }
+
+  convertObservationToFormGroup(observation: Observation): FormGroup {
+    const group: { [fieldId: string]: FormControl } = {};
+    for (const [fieldId, field] of observation.form!.fields) {
+      const response = observation!.responses?.get(fieldId);
+      switch (field.type) {
+        case FieldType.TEXT:
+          this.addControlsForTextField(group, field, response);
+          break;
+        case FieldType.MULTIPLE_CHOICE:
+          this.addControlsForMultipleChoiceField(group, field, response);
+          break;
+        default:
+          throw Error(
+            `Unimplemented conversion to FormControl(s) for Field with
+             Type:${field.type}`
+          );
+      }
+    }
+    return this.formBuilder.group(group);
+  }
+
+  extractResponses(): Map<string, Response> {
+    return new Map<string, Response>(
+      this.observationFields!.map(field => [
+        field.id,
+        this.extractResponseForField(field),
+      ])
+    );
+  }
+
+  extractResponseForField(field: Field) {
+    switch (field.type) {
+      case FieldType.TEXT:
+        return this.extractResponseForTextField(field);
+      case FieldType.MULTIPLE_CHOICE:
+        return this.extractResponseForMultipleChoiceField(field);
+      default:
+        throw Error(
+          `Unimplemented Response extraction for Field with
+           Type:${field.type}`
+        );
+    }
+  }
+
+  addControlsForTextField(
+    group: { [fieldId: string]: FormControl },
+    field: Field,
+    response?: Response
+  ): void {
+    const value = response?.value as string;
+    group[field.id] = field.required
+      ? new FormControl(value, Validators.required)
+      : new FormControl(value);
+  }
+
+  extractResponseForTextField(field: Field): Response {
+    return new Response(this.observationForm?.value[field.id]);
+  }
+
+  addControlsForMultipleChoiceField(
+    group: { [fieldId: string]: FormControl },
+    field: Field,
+    response?: Response
+  ): void {
+    switch (field.multipleChoice?.cardinality) {
+      case Cardinality.SELECT_ONE:
+        this.addControlsForSelectOneField(group, field, response);
+        return;
+      case Cardinality.SELECT_MULTIPLE:
+        this.addControlsForSelectMultipleField(group, field, response);
+        return;
+      default:
+        throw Error(
+          `Unimplemented conversion to FormControl(s) for Field with
+           Cardinality:${field.multipleChoice?.cardinality}`
+        );
+    }
+  }
+
+  extractResponseForMultipleChoiceField(field: Field): Response {
+    switch (field.multipleChoice?.cardinality) {
+      case Cardinality.SELECT_ONE:
+        return this.extractResponseForSelectOneField(field);
+      case Cardinality.SELECT_MULTIPLE:
+        return this.extractResponseForSelectMultipleField(field);
+      default:
+        throw Error(
+          `Unimplemented Response extraction for Field with
+           Cardinality:${field.multipleChoice?.cardinality}`
+        );
+    }
+  }
+
+  addControlsForSelectOneField(
     group: { [fieldId: string]: FormControl },
     field: Field,
     response?: Response
@@ -63,86 +165,28 @@ export class ObservationFormComponent {
       : new FormControl(selectedOptionId);
   }
 
-  populateFormGroupWithControlsForSelectMultipleField(
+  extractResponseForSelectOneField(field: Field): Response {
+    const selectedOption: Option = field.getMultipleChoiceOption(
+      this.observationForm?.value[field.id]
+    );
+    return new Response(List([selectedOption]));
+  }
+
+  addControlsForSelectMultipleField(
     group: { [fieldId: string]: FormControl },
     field: Field,
     response?: Response
   ): void {
     const selectedOptions = response?.value as List<Option>;
     for (const option of field.multipleChoice!.options) {
-      group[option.id] = new FormControl(
-        selectedOptions?.contains(option) || false
-      );
+      group[option.id] = new FormControl(selectedOptions?.contains(option));
     }
   }
 
-  populateFormGroupWithControlsForMultipleChoiceField(
-    group: { [fieldId: string]: FormControl },
-    field: Field,
-    response?: Response
-  ): void {
-    switch (field.multipleChoice?.cardinality) {
-      case Cardinality.SELECT_ONE:
-        this.populateFormGroupWithControlsForSelectOneField(
-          group,
-          field,
-          response
-        );
-        return;
-      case Cardinality.SELECT_MULTIPLE:
-        this.populateFormGroupWithControlsForSelectMultipleField(
-          group,
-          field,
-          response
-        );
-        return;
-      default:
-        throw Error(
-          `Can not create FormControl(s) for given Cardinality:${field.multipleChoice?.cardinality}`
-        );
-    }
-  }
-
-  populateFormGroupWithControlsForTextField(
-    group: { [fieldId: string]: FormControl },
-    field: Field,
-    response?: Response
-  ): void {
-    const value = response?.value as string;
-    group[field.id] = field.required
-      ? new FormControl(value, Validators.required)
-      : new FormControl(value);
-  }
-
-  getFormGroupForObservation(observation?: Observation): FormGroup {
-    const group: { [fieldId: string]: FormControl } = {};
-    for (const [fieldId, field] of observation!.form!.fields) {
-      const fieldResponse = observation!.responses?.get(fieldId);
-      switch (field.type) {
-        case FieldType.TEXT:
-          this.populateFormGroupWithControlsForTextField(
-            group,
-            field,
-            fieldResponse
-          );
-          break;
-        case FieldType.MULTIPLE_CHOICE:
-          this.populateFormGroupWithControlsForMultipleChoiceField(
-            group,
-            field,
-            fieldResponse
-          );
-          break;
-        default:
-          throw Error(
-            `Can not create FormControl(s) for given FieldType:${field.type}`
-          );
-      }
-    }
-    return this.formBuilder.group(group);
-  }
-
-  onSave() {
-    this.payLoad = JSON.stringify(this.observationForm?.getRawValue());
+  extractResponseForSelectMultipleField(field: Field): Response {
+    const selectedOptions: List<Option> = field.multipleChoice!.options!.filter(
+      option => this.observationForm?.value[option.id]
+    );
+    return new Response(selectedOptions);
   }
 }
