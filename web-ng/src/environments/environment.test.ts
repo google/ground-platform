e64a5da9b90d1f46a26fdd0e21df7d8a07a38446
Gino Miceli <gmiceli@google.com>
2020-05-10 20:29:43 -0400

    Add test values for required Firebase config

--- web-ng/src/environments/environment.test.ts
+++ web-ng/src/environments/environment.test.ts
@@ -23,10 +23,10 @@ export const environment = {
   production: false,
   googleMapsApiKey: '',
   firebaseConfig: {
-    apiKey: '',
+    apiKey: 'test',
     authDomain: '',
     databaseURL: '',
-    projectId: '',
+    projectId: 'test',
     storageBucket: '',
     messagingSenderId: '',
     appId: '',
