c7de0c72c125b075e8a1db5dc04ae13e25f070dc
Daoyu Tu <daoyu@google.com>
2020-06-10 15:45:43 -0400

    Use custom Ground marker for default style button.

--- web-ng/src/app/components/edit-style-button/edit-style-button.component.ts
+++ web-ng/src/app/components/edit-style-button/edit-style-button.component.ts
@@ -20,9 +20,12 @@ import {
   EventEmitter,
   Input,
   Output,
+  OnInit,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { ColorPickerComponent } from '../color-picker/color-picker.component';
+import { getPinImageSource } from '../map/ground-pin';
 import { ColorEvent } from 'ngx-color';
 
 @Component({
@@ -30,14 +33,26 @@ import { ColorEvent } from 'ngx-color';
   templateUrl: './edit-style-button.component.html',
   styleUrls: ['./edit-style-button.component.css'],
 })
-export class EditStyleButtonComponent {
+export class EditStyleButtonComponent implements OnInit {
   @Input() markerColor = 'black';
   @Output() markerColorChange = new EventEmitter<MarkerColorEvent>();
+  markerPinUrl: SafeUrl;
 
   constructor(
     // tslint:disable-next-line:no-any
-    private dialog: MatDialog
-  ) {}
+    private dialog: MatDialog,
+    private sanitizer: DomSanitizer
+  ) {
+    this.markerPinUrl = sanitizer.bypassSecurityTrustUrl(
+      getPinImageSource(this.markerColor)
+    );
+  }
+
+  ngOnInit() {
+    this.markerPinUrl = this.sanitizer.bypassSecurityTrustUrl(
+      getPinImageSource(this.markerColor)
+    );
+  }
 
   openColorPickerDialog(evt: MouseEvent): void {
     const target = new ElementRef(evt.currentTarget);
@@ -48,6 +63,9 @@ export class EditStyleButtonComponent {
       .componentInstance.onColorPicked.subscribe((evt: ColorEvent) => {
         this.markerColor = evt.color.hex;
         this.markerColorChange.emit(new MarkerColorEvent(this.markerColor));
+        this.markerPinUrl = this.sanitizer.bypassSecurityTrustUrl(
+          getPinImageSource(this.markerColor)
+        );
       });
   }
 }
