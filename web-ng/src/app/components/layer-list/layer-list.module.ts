3de04326fb2a77f8a5c922d800ff6dea1545f746
Gaurav Chalana <gauravchalana.ece@gmail.com>
2020-07-01 00:23:11 -0500

    Add Layer button

--- web-ng/src/app/components/layer-list/layer-list.module.ts
+++ web-ng/src/app/components/layer-list/layer-list.module.ts
@@ -19,9 +19,17 @@ import { NgModule } from '@angular/core';
 import { LayerListComponent } from './layer-list.component';
 import { LayerListItemModule } from '../layer-list-item/layer-list-item.module';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [BrowserModule, MatListModule, LayerListItemModule],
+  imports: [
+    BrowserModule,
+    MatListModule,
+    LayerListItemModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   exports: [LayerListComponent],
   declarations: [LayerListComponent],
 })
