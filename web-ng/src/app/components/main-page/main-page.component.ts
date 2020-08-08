9e19a627cc2560cd93c968272f6bd56171ce57cd
parulraheja98 <parulraheja98@gmail.com>
2020-08-08 01:15:36 -0500

    Add global constant LAYER_ID_NEW

--- web-ng/src/app/components/main-page/main-page.component.ts
+++ web-ng/src/app/components/main-page/main-page.component.ts
@@ -31,6 +31,9 @@ import { ActivatedRoute } from '@angular/router';
  * project header. Responsible for coordinating page-level URL states with
  * various services.
  */
+
+const LAYER_ID_NEW = ':new';
+
 @Component({
   selector: 'ground-main-page',
   templateUrl: './main-page.component.html',
@@ -88,7 +91,7 @@ export class MainPageComponent implements OnInit {
   private showEditLayerDialog(layerId: string) {
     this.activeProject$.pipe(take(1)).subscribe(project =>
       this.dialog.open(LayerDialogComponent, {
-        autoFocus: layerId === ':new',
+        autoFocus: layerId === LAYER_ID_NEW,
         data: {
           projectId: project.isUnsavedNew()
             ? Project.PROJECT_ID_NEW
