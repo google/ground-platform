95256d836762c09526b5bec9677eeb5b3944235b
Gino Miceli <gmiceli@google.com>
2020-08-19 14:22:32 -0400

    Comments and suggestions

--- web-ng/src/app/components/form-field-editor/form-field-editor.component.ts
+++ web-ng/src/app/components/form-field-editor/form-field-editor.component.ts
@@ -52,11 +52,15 @@ export interface FieldTypeSelectOption {
 export class FormFieldEditorComponent implements OnInit, OnChanges {
   @Input() label?: string;
   @Input() required?: boolean;
+  // Could this be an enum instead of a string so that it's more self-
+  // documenting and also compile checked?
   @Input() type?: string;
   @Input() multipleChoice?: MultipleChoice;
   @Output() update = new EventEmitter();
   @Output() delete = new EventEmitter();
   formOptions: MultipleChoice | undefined;
+  // Once would expect a variable called `fieldTypes` to be of type `FieldType`,
+  // whereas here it reflects form select field options.
   fieldTypes: FieldTypeSelectOption[] = [
     {
       icon: 'short_text',
@@ -74,29 +78,47 @@ export class FormFieldEditorComponent implements OnInit, OnChanges {
 
   constructor(
     private formBuilder: FormBuilder,
+    // Unused, can be removed.
     private dataStoreService: DataStoreService,
     private confirmationDialog: MatDialog,
     private layerService: LayerService
   ) {
+    // For consistency, can this be defined right after fieldTypes rather
+    // than in the constructor (or vice versa)?
+    // Re naming - we're already in the FormFieldEditor - since there's only
+    // one of these, 'formGroup' would also be sufficient.
     this.formFieldGroup = this.formBuilder.group({
       label: ['', Validators.required],
       required: [false],
+      // What's special about value "0" - add a comment explaining that we
+      // define to the 0th item (i.e. "text" is the default option).
+      // Also - `type` is overloaded here to be both the field type
+      // (FieldType.TEXT) and the selected field type option. Careful
+      // naming can disambiguate here.
       type: this.fieldTypes[0],
     });
   }
 
   ngOnInit(): void {
     // As the form fields value change we are emitting the updated value to the layer-dialog.
+    // We need to store the subscription here returned as we do in other
+    // components and dispose of it in ngOnDestroy().
     this.formFieldGroup.valueChanges.subscribe(value => {
       this.update.emit({
         label: StringMap({ en: value.label }),
         required: value.required,
+        // This is hard to grok, it's a signal that the names could be chosen
+        // to more carefully reflect what's what.
         type: value.type.type,
+        // Same here. What's a "multipleChoice" and why is it the same thing
+        // as formOptions? As-is it's not possible to understand what's
+        // happening here without tracing back through the code.
         multipleChoice: this.formOptions,
       });
     });
   }
 
+  // A comment explaining when is this method called would be helpful.
   ngOnChanges(changes: SimpleChanges) {
     const type = this.fieldTypes.find(
       fieldType => fieldType.type === Number(this.type)
@@ -104,6 +126,9 @@ export class FormFieldEditorComponent implements OnInit, OnChanges {
     if (changes.multipleChoice) {
       this.formOptions = this.multipleChoice;
       const options = this.formOptions?.options;
+      // Does this sort the actual options in the form or a copy of them?
+      // If it's the actual options, may be worth inlining the var `options`
+      // here to make it more obvious.
       options?.sortBy(option => option.index);
     }
     this.formFieldGroup.setValue({
@@ -144,19 +169,27 @@ export class FormFieldEditorComponent implements OnInit, OnChanges {
     }
   }
 
+  // This method has an obscure name and I don't see a reference to it in this
+  // class; if the name has special meaning a comment would help clarify.
   trackByFn(index: number) {
     return index;
   }
 
+  // Original wording implied that the method is run many times, or once, and
+  // sits around waiting until the label or code is updated. Instead, it
+  // actually only called when that happens, and the method itself doesn't
+  // impose those conditions at all. Also, the method itself doesn't only emit
+  // to the LayerDialog; that's just the only subscriber to the component's
+  // output. A more precise ("technical correct") description will prevent
+  // confusion.
   /**
-   * Emits event to layer dialog whenvever label or code gets updated in options-editor.
+   * Emits event with updated Options. Called when label or code gets updated.
    *
    * @param event: label and code value of the option field.
    * @param index: index of the option to be updated.
    * @returns void
    *
    */
-
   onOptionUpdate(event: { label: string; code: string }, index: number) {
     const option = this.layerService.createOption(
       event.code,
@@ -169,6 +202,8 @@ export class FormFieldEditorComponent implements OnInit, OnChanges {
 
   onOptionDelete(index: number) {
     const dialogRef = this.openConfirmationDialog();
+    // Let's use toPromise().then() here since this only emits once. It also
+    // avoids us needing to manage the subscription.
     dialogRef.afterClosed().subscribe(dialogResult => {
       if (dialogResult) {
         let options = this.formOptions?.options;
@@ -185,6 +220,7 @@ export class FormFieldEditorComponent implements OnInit, OnChanges {
       data: {
         title: 'Warning',
         message:
+          // Split up long lines.
           'Are you sure you wish to delete this option? Any associated data will be lost. This cannot be undone.',
       },
     });
