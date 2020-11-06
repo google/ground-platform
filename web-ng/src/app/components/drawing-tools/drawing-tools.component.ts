9416ced05f2f164101ad0d895a0001c1167bfa6a
Gino Miceli <gmiceli@google.com>
2020-11-06 18:39:41 +0200

    Revert "Simplify drawing tools component"
    This reverts commit 2c50832694d87081935f6a503151b502f40c3bff.

--- web-ng/src/app/components/drawing-tools/drawing-tools.component.ts
+++ web-ng/src/app/components/drawing-tools/drawing-tools.component.ts
@@ -39,7 +39,7 @@ export class DrawingToolsComponent {
   selectedValue = '';
   private lastSelectedValue = '';
   selectedLayerId = '';
-  readonly layers: List<Layer>;
+  readonly layers$: Observable<List<Layer>>;
   readonly lang: string;
 
   constructor(
@@ -49,11 +49,12 @@ export class DrawingToolsComponent {
   ) {
     // TODO: Make dynamic to support i18n.
     this.lang = 'en';
-    const project = projectService.getActiveProject()!;
-    this.selectedLayerId = project.layers.keySeq().first();
-    this.drawingToolsService.setSelectedLayerId(this.selectedLayerId);
-    this.layers = List(project.layers.valueSeq().toArray()).sortBy(
-      l => l.index
+    this.layers$ = projectService.getActiveProject$().pipe(
+      map(project => {
+        this.selectedLayerId = project.layers.keySeq().first();
+        this.drawingToolsService.setSelectedLayerId(this.selectedLayerId);
+        return List(project.layers.valueSeq().toArray()).sortBy(l => l.index);
+      })
     );
   }
 
