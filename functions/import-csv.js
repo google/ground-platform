51c3689a6fd2b2954719416561a257ea4faaafc3
Gino Miceli <gmiceli@google.com>
2020-11-29 22:30:33 +0200

    Add support for custom attributes

--- functions/import-csv.js
+++ functions/import-csv.js
@@ -15,58 +15,136 @@
  * limitations under the License.
  */
 
-'use strict';
+"use strict";
 
 const HttpStatus = require("http-status-codes");
-const { firestore } = require('firebase-admin');
+const { firestore } = require("firebase-admin");
 const csvParser = require("csv-parser");
 const Busboy = require("busboy");
 const { db } = require("./common/context");
 
+const closeConnection = (code, reason) => {
+  req.unpipe(busboy);
+  res.writeHead(code || 400, { Connection: "close" });
+  res.end(reason || "Bad Request");
+};
+
+/**
+ * Streams a multipart HTTP POSTed form containing a CSV 'file' and required
+ * 'project' id and 'layer' id to the database.
+ */
 function importCsv(req, res) {
-    // Based on https://cloud.google.com/functions/docs/writing/http#multipart_data
-    if (req.method !== "POST") {
-        return res.status(HttpStatus.METHOD_NOT_ALLOWED).end();
+  // Based on https://cloud.google.com/functions/docs/writing/http#multipart_data
+  if (req.method !== "POST") {
+    return res.status(HttpStatus.METHOD_NOT_ALLOWED).end();
+  }
+  const busboy = new Busboy({ headers: req.headers });
+
+  // Dictionary used to accumulate form field values, keyed by field name.
+  const params = {};
+
+  // Handle non-file fields in the form. project and layer must appear
+  // before the file for the file handler to work properly.
+  busboy.on("field", (key, val) => {
+    params[key] = val;
+  });
+
+  // This code will process each file uploaded.
+  busboy.on("file", (key, file, _) => {
+    const { project: projectId, layer: layerId } = params;
+    if (!projectId || !layerId) {
+      return res.status(HttpStatus.BAD_REQUEST).end();
     }
-    const busboy = new Busboy({ headers: req.headers });
+    console.log(
+      `Importing features into project '${projectId}', layer '${layerId}'..`
+    );
 
-    // Dictionary used to accumulate form field values, keyed by field name.
-    const params = {};
+    // Pipe file through CSV parser lib, inserting each row in the db as it is
+    // received.
 
-    // Handle non-file fields in the form. projectId and layerId must appear
-    // before the file for the file handler to work properly.
-    busboy.on("field", (key, val) => { params[key] = val; });
-    // This code will process each file uploaded.
-    busboy.on("file", (key, file, filename) => {
-        const { projectId, layerId } = params;
-        if (key != "csvfile" || !projectId || !layerId) {
-            return res.status(HttpStatus.BAD_REQUEST).end();
-        }
-        console.log(
-            `Importing features into project '${projectId}', layer '${layerId}'..`);
-
-        // Pipe file through CSV parser lib, inserting each row in the db as it is
-        // received.
-        file.pipe(csvParser()).on("data", async (record) => {
-            console.log("Processing row: ", JSON.stringify(record));
-            await db.insertFeature(projectId, layerId, csvRowToFeature(record));
-        });
+    file.pipe(csvParser()).on("data", async (row) => {
+      try {
+        await insertRow(projectId, layerId, row);
+      } catch (err) {
+        console.error(err);
+        await res.status(HttpStatus.BAD_REQUEST).end("{}");
+        // TODO(#525): Abort stream on error. How?
+      }
     });
+  });
 
-    // Triggered once all uploaded files are processed by Busboy.
-    busboy.on("finish", async () => {
-        res.sendStatus(HttpStatus.OK);
-    });
-    busboy.end(req.rawBody);
+  // Triggered once all uploaded files are processed by Busboy.
+  busboy.on("finish", async () => {
+    // TODO(#525): Return more meaningful errors.
+    await res.status(HttpStatus.OK).end("{}");
+  });
+
+  busboy.on("error", (err) => {
+    console.err("Busboy error", err);
+    next(err);
+  });
+  busboy.end(req.rawBody);
 }
 
-function csvRowToFeature(record) {
-    var feature = {
-        caption: record.name + "," + record.state,
-        location: new firestore.GeoPoint(Number.parseFloat(record.lat)
-            , Number.parseFloat(record.long))
-    };
-    return feature;
+/**
+ * Transforms a dictionary of string in the form A->B[] into a dictionary of
+ * strings in the form B->A. Values in B[] are assumed to appear at most once
+ * in the array values in the the provided dictionary.
+ */
+function invertAndFlatten(obj) {
+  return Object.keys(obj)
+    .flatMap((k) => obj[k].map((v) => ({ k, v })))
+    .reduce((o, { v, k }) => {
+      o[v] = k;
+      return o;
+    }, {});
+}
+
+/**
+ * Dictionary of lowercase aliases for recognized feature attributes. Case
+ * is ignored when mapping column aliases to feature attributes.
+ */
+const SPECIAL_COLUMN_NAMES = invertAndFlatten({
+  id: ["id", "key"],
+  caption: ["caption", "name", "label"],
+  lat: ["lat", "latitude", "y"],
+  lng: ["lng", "lon", "long", "lng", "x"],
+});
+
+async function insertRow(projectId, layerId, record) {
+  const feature = csvRowToFeature(record, layerId);
+  if (feature) {
+    await db.insertFeature(projectId, feature);
+  }
+}
+
+/**
+ * Convert the provided row (key-value pairs) and layerId into a Feature for
+ * insertion into the data store.
+ */
+function csvRowToFeature(row, layerId) {
+  let data = { layerId };
+  let attributes = {};
+  for (const key in row) {
+    const featureKey = SPECIAL_COLUMN_NAMES[key.toLowerCase()];
+    const value = row[key];
+    if (featureKey) {
+      data[featureKey] = value;
+    } else {
+      attributes[key] = value;
+    }
+  }
+  let { lat, lng, ...feature } = data;
+  lat = Number.parseFloat(lat);
+  lng = Number.parseFloat(lng);
+  if (isNaN(lat) || isNaN(lng)) {
+    return null;
+  }
+  feature["location"] = new firestore.GeoPoint(lat, lng);
+  if (Object.keys(attributes).length > 0) {
+    feature["attributes"] = attributes;
+  }
+  return feature;
 }
 
 module.exports = importCsv;
