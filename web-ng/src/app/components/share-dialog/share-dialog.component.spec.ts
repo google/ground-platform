c904d9b46447485997a927a4763c6e247c0d1d94
Gino Miceli <gmiceli@google.com>
2020-11-30 14:00:34 +0200

    Fix share dialog tests

--- web-ng/src/app/components/share-dialog/share-dialog.component.spec.ts
+++ web-ng/src/app/components/share-dialog/share-dialog.component.spec.ts
@@ -15,15 +15,19 @@
  */
 
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
 import { ShareDialogComponent } from './share-dialog.component';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { ProjectService } from '../../services/project/project.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 import { Map } from 'immutable';
-import { MatOptionModule } from '@angular/material/core';
 
 describe('ShareDialogComponent', () => {
   let component: ShareDialogComponent;
@@ -33,7 +37,18 @@ describe('ShareDialogComponent', () => {
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [ShareDialogComponent],
-        imports: [MatFormFieldModule, MatSelectModule, MatOptionModule],
+        imports: [
+          NoopAnimationsModule,
+          FlexLayoutModule,
+          MatButtonModule,
+          MatDialogModule,
+          MatFormFieldModule,
+          MatInputModule,
+          MatListModule,
+          MatSelectModule,
+          FormsModule,
+          ReactiveFormsModule,
+        ],
         providers: [
           { provide: MatDialogRef, useValue: {} },
           {
