97e754bac85ef951c14421fe7e813307124fa567
Gaurav Chalana <gauravchalana.ece@gmail.com>
2020-12-16 00:43:07 -0600

    Fix test

--- web-ng/src/app/components/feature-details/feature-details.component.spec.ts
+++ web-ng/src/app/components/feature-details/feature-details.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { NavigationService } from '../../services/router/router.service';
 
 import { FeatureDetailsComponent } from './feature-details.component';
 
@@ -7,10 +16,20 @@ describe('FeatureDetailsComponent', () => {
   let fixture: ComponentFixture<FeatureDetailsComponent>;
 
   beforeEach(async () => {
+    const navigationService = {
+      getProjectId$: () => of(''),
+      getFeatureId$: () => of(''),
+    };
     await TestBed.configureTestingModule({
-      declarations: [ FeatureDetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [FeatureDetailsComponent],
+      imports: [MatIconModule, MatListModule, MatMenuModule, MatDialogModule],
+      providers: [
+        { provide: Router, useValue: {} },
+        { provide: AngularFirestore, useValue: {} },
+        { provide: AngularFireAuth, useValue: {} },
+        { provide: NavigationService, useValue: navigationService },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
