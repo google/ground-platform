4dcb5f4d70d56ac5d35d6ccc6531248b44bc45d6
Heman Parbhakar <hemanp@google.com>
2021-06-03 15:38:33 +0530

    Copyright added to the entire card-view-component

--- web-ng/src/app/components/card-view-project/card-view-project.component.ts
+++ web-ng/src/app/components/card-view-project/card-view-project.component.ts
@@ -1,15 +1,28 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { Component, OnDestroy, ElementRef, OnInit } from '@angular/core';
 
 import { ProjectService } from '../../services/project/project.service';
-import { Role } from '../../shared/models/role.model';
 import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { UserProfilePopupComponent } from '../../components/user-profile-popup/user-profile-popup.component';
 import { Project } from '../../shared/models/project.model';
 import { NavigationService } from '../../services/navigation/navigation.service';
-import { AclEntry } from '../../shared/models/acl-entry.model';
 import { AuthService } from '../../services/auth/auth.service';
-import { remove } from 'immutable';
 
 @Component({
   selector: 'app-card-view-project',
