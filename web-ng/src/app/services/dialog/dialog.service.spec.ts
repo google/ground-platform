ba5b097c669566c579a145f191cf80af23b329a7
os-micmec <micmec@google.com>
2021-11-11 18:26:00 +0100

    Add tests to DialogService

--- web-ng/src/app/services/dialog/dialog.service.spec.ts
+++ web-ng/src/app/services/dialog/dialog.service.spec.ts
@@ -17,17 +17,47 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DialogService } from './dialog.service';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { ConfirmationDialogComponent } from '../../components/confirmation-dialog/confirmation-dialog.component';
 
 describe('DialogService', () => {
   let service: DialogService;
+  let dialogSpy: jasmine.Spy;
+  const dialogRefSpyObj = jasmine.createSpyObj({
+    afterClosed: of({}),
+    close: null,
+  });
+  dialogRefSpyObj.componentInstance = { body: '' }; // attach componentInstance to the spy object...
 
   beforeEach(() => {
     TestBed.configureTestingModule({ imports: [MatDialogModule] });
     service = TestBed.inject(DialogService);
   });
 
+  beforeEach(() => {
+    dialogSpy = spyOn(TestBed.inject(MatDialog), 'open').and.returnValue(
+      dialogRefSpyObj
+    );
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return correct dialog without discard actions', () => {
+    service.openConfirmationDialog('testTitle', 'testMessage');
+
+    expect(dialogSpy).toHaveBeenCalled();
+
+    expect(dialogSpy).toHaveBeenCalledWith(ConfirmationDialogComponent, {
+      maxWidth: '500px',
+      autoFocus: false,
+      data: {
+        title: 'testTitle',
+        message: 'testMessage',
+        showDiscardActions: false,
+      },
+    });
   });
 });
