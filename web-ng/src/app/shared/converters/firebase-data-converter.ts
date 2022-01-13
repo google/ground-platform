bc3a1f7a5eff910cceeeeb0ac6786d4302b5f437
Gino Miceli <228050+gino-m@users.noreply.github.com>
2022-01-13 10:18:40 -0500

    Revert type check

--- web-ng/src/app/shared/converters/firebase-data-converter.ts
+++ web-ng/src/app/shared/converters/firebase-data-converter.ts
@@ -521,7 +521,7 @@ export class FirebaseDataConverter {
     if (typeof responseValue === 'number') {
       return new Response(responseValue as number);
     }
-    if (responseValue instanceof List) {
+    if (responseValue instanceof Array) {
       return new Response(
         List(
           responseValue.map(optionId =>
