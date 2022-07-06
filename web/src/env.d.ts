3e1e5f8d7d7599885be3cc4a6bf9af3244290382
Michelangelo Mecozzi <micmec@google.com>
2022-07-06 19:13:10 -0400

    Add continuous deployment action

---
+++ web/src/env.d.ts
@@ -0,0 +1,15 @@
+declare var process: {
+  env: {
+    NG_APP_ENV: string;
+    GOOGLE_MAPS_API_KEY: string;
+    FIREBASE_CONFIG_API_KEY: string;
+    FIREBASE_CONFIG_AUTH_DOMAIN: string;
+    FIREBASE_CONFIG_DATABASE_URL: string;
+    FIREBASE_CONFIG_PROJECT_ID: string;
+    FIREBASE_CONFIG_STORAGE_BUCKET: string;
+    FIREBASE_CONFIG_MESSAGING_SENDER_ID: string;
+    FIREBASE_CONFIG_APP_ID: string;
+    CLOUD_FUNCTIONS_URL: string;
+    OFFLINE_BASE_MAP_SOURCES_URL: string;
+  };
+};
