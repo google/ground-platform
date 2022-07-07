50268473144768338c4f9d509e109b53636e83e7
os-micmec <micmec@google.com>
2022-07-07 17:31:17 -0400

    Fix 'process is undefined' error

--- web/src/polyfills.ts
+++ web/src/polyfills.ts
@@ -76,3 +76,7 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
