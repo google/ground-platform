ccea145ee18de08b00b7d48dc67998accdc197bf
gino-m <gmiceli@google.com>
2022-11-29 18:09:54 -0500

    Fix checks

--- web/precheck.sh
+++ web/precheck.sh
@@ -13,6 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo 'export const googleMapsConfig = {};' > src/environments/.google-maps-config.ts
-echo 'export const firebaseConfig = {};' > src/environments/.firebase-config.ts
-echo '{"offlineBaseMapSources": {}}' > src/environments/.backend-config.json
+echo 'export const googleMapsConfig = {"apiKey": ""};' > src/environments/.google-maps-config.ts
+echo 'export const firebaseConfig = {"projectId": ""};' > src/environments/.firebase-config.ts
+echo '{"offlineBaseMapSources": []}' > src/environments/.backend-config.json
