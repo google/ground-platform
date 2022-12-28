4ffb3770015fc5f8f6d0591eec58f1d00b4000be
Gino Miceli <gmiceli@google.com>
2022-12-28 17:12:28 -0500

    Use related paths for nested page components

--- web/src/app/pages/main-page-container/main-page-container.component.spec.ts
+++ web/src/app/pages/main-page-container/main-page-container.component.spec.ts
@@ -17,8 +17,8 @@
 import { ActivatedRouteStub } from 'testing/activated-route-stub';
 import { ActivatedRoute } from '@angular/router';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { MainPageContainerComponent } from 'app/pages/main-page-container/main-page-container.component';
-import { MainPageComponent } from 'app/pages/main-page-container/main-page/main-page.component';
+import { MainPageContainerComponent } from './main-page-container.component';
+import { MainPageComponent } from './main-page/main-page.component';
 import { NavigationService } from 'app/services/navigation/navigation.service';
 import { NEVER } from 'rxjs';
 import { SurveyService } from 'app/services/survey/survey.service';
--- web/src/app/pages/main-page-container/main-page/main-page.module.ts
+++ web/src/app/pages/main-page-container/main-page/main-page.module.ts
@@ -18,15 +18,15 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { JobDialogModule } from 'app/components/job-dialog/job-dialog.module';
 import { DrawingToolsModule } from 'app/components/drawing-tools/drawing-tools.module';
-import { MainPageComponent } from 'app/pages/main-page-container/main-page/main-page.component';
+import { MainPageComponent } from './main-page.component';
 import { MapModule } from 'app/components/map/map.module';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgModule } from '@angular/core';
-import { SidePanelModule } from 'app/pages/main-page-container/main-page/side-panel/side-panel.module';
-import { SurveyHeaderModule } from 'app/pages/main-page-container/main-page/survey-header/survey-header.module';
+import { SidePanelModule } from './side-panel/side-panel.module';
+import { SurveyHeaderModule } from './survey-header/survey-header.module';
 import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
