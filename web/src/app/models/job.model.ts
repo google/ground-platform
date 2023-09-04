4eaaa14a8ef6133a7ea58f2015023bbba953fa6b
Gino Miceli <gmiceli@google.com>
2023-09-04 16:02:19 -0400

    Fix merge error

--- web/src/app/models/job.model.ts
+++ web/src/app/models/job.model.ts
@@ -29,11 +29,4 @@ export class Job extends Copiable {
   ) {
     super();
   }
-
-  copyWith(modifyObject: {[P in keyof this]?: this[P]}): this {
-    return Object.assign(Object.create(this.constructor.prototype), {
-      ...this,
-      ...modifyObject,
-    });
- }
 }
