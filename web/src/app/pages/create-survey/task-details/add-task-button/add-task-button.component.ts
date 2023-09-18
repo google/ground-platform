eb80b2d465717059b9aa30a9af29ede6075b700f
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2023-09-18 16:44:22 +0200

    added missing buttons types

--- web/src/app/pages/create-survey/task-details/add-task-button/add-task-button.component.ts
+++ web/src/app/pages/create-survey/task-details/add-task-button/add-task-button.component.ts
@@ -38,6 +38,18 @@ export class AddTaskButtonComponent {
         this.text = 'Take a photo';
         this.icon = 'photo_camera';
         break;
+      case TaskGroup.DROP_PIN:
+        this.text = 'Drop a pin';
+        this.icon = 'pin_drop';
+        break;
+      case TaskGroup.DRAW_AREA:
+        this.text = 'Draw an area';
+        this.icon = 'draw';
+        break;
+      case TaskGroup.SUGGEST_LOI:
+        this.text = 'Capture location';
+        this.icon = 'share_location';
+        break;
       default:
         this.text = 'Undefined Task';
         this.icon = 'question_mark';
