29fdd485d1b325b1df1c70b1c578a75926c6d7d5
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-01-05 11:10:22 +0100

    Job list is now ordered by id

--- web/src/app/pages/edit-survey/edit-survey.component.ts
+++ web/src/app/pages/edit-survey/edit-survey.component.ts
@@ -89,9 +89,11 @@ export class EditSurveyComponent implements OnInit {
   }
 
   jobs(): Job[] {
-    // TODO: sort by id or remove sort call when task editor is fixed
-    return Array.from(this.survey?.jobs.values() ?? []).sort(
-      ({id: id1}, {id: id2}) => id1.localeCompare(id2)
+    return (
+      this.survey?.jobs
+        .toIndexedSeq()
+        .toArray()
+        .sort(({index: id1}, {index: id2}) => (id1 < id2 ? -1 : 1)) || []
     );
   }
 
