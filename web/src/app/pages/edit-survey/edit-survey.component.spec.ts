9f45a1add43abc57191d24e7846b7f4225051f6d
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-01-23 14:23:31 +0100

    small optimisations

--- web/src/app/pages/edit-survey/edit-survey.component.spec.ts
+++ web/src/app/pages/edit-survey/edit-survey.component.spec.ts
@@ -264,7 +264,7 @@ describe('EditSurveyComponent', () => {
         expect(draftSurveyServiceSpy.addOrUpdateJob).toHaveBeenCalledOnceWith(
           job1.copyWith({
             id: newJob.id,
-            name: 'Copy of ' + job1.name,
+            name: `Copy of ${job1.name}`,
             color: undefined,
           }),
           true
