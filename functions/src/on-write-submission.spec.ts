742d902202b2fb874c5b3602c1675c65b49f7e0c
Gino Miceli <gmiceli@google.com>
2024-06-24 16:55:47 +0200

    Tests WIP

--- functions/src/on-write-submission.spec.ts
+++ functions/src/on-write-submission.spec.ts
@@ -17,14 +17,14 @@
 import {
   stubAdminApi,
   newEventContext,
-  newDocumentSnapshot
+  newDocumentSnapshot,
+  mockFirestore
 } from '@ground/lib/dist/testing/firestore';
+import * as functions from './index';
 
 const test = require('firebase-functions-test')();
 
 describe('onWriteSubmission()', () => {
-  let functions: any;
-
   const SURVEY_ID = 'survey1';
   const SUBMISSION = newDocumentSnapshot({loiId: 'loi1'});
   const CONTEXT = newEventContext({surveyId: SURVEY_ID});
@@ -35,8 +35,6 @@ describe('onWriteSubmission()', () => {
 
   beforeAll(() => {
     stubAdminApi();
-    // Module must be loaded after Admin API has been stubbed.
-    functions = require('./index');
   });
 
   afterAll(() => {
@@ -48,66 +46,57 @@ describe('onWriteSubmission()', () => {
     loiId: string,
     count: number
   ) {
-    // testFirestore.collection.withArgs(submissionsPath).and.returnValue({
-    //   where: jasmine
-    //     .createSpy('where')
-    //     .withArgs('loiId', '==', loiId)
-    //     .and.returnValue(newCountQuery(count)),
-    // });
+    mockFirestore.collection(SUBMISSIONS_PATH).doc('001').set({
+      a: 'a'
+    });
   }
 
-  function installLoiUpdateSpy(loiPath: string) {
-    const loiUpdateSpy = jasmine.createSpy('update');
-    // testFirestore.doc.withArgs(loiPath).and.returnValue({update: loiUpdateSpy});
-    return loiUpdateSpy;
-  }
 
-  it('update submission count on create', async () => {
+  fit('update submission count on create', async () => {
     installSubmissionCountSpy(SUBMISSIONS_PATH, LOI_ID, 2);
-    const loiUpdateSpy = installLoiUpdateSpy(LOI_PATH);
 
     await test.wrap(functions.onWriteSubmission)(
       {before: undefined, after: SUBMISSION},
       CONTEXT
     );
 
-    expect(loiUpdateSpy).toHaveBeenCalledOnceWith({submissionCount: 2});
+    const loi = await mockFirestore.doc(LOI_PATH).get();
+    expect(loi.data()).toEqual({submissionCount: 2});
   });
 
   it('update submission count on delete', async () => {
     installSubmissionCountSpy(SUBMISSIONS_PATH, LOI_ID, 1);
-    const loiUpdateSpy = installLoiUpdateSpy(LOI_PATH);
 
     await test.wrap(functions.onWriteSubmission)(
       {before: SUBMISSION, after: undefined},
       CONTEXT
     );
 
-    expect(loiUpdateSpy).toHaveBeenCalledOnceWith({submissionCount: 1});
+    const loi = await mockFirestore.doc(LOI_PATH).get();
+    expect(loi.data()).toEqual({submissionCount: 1});
   });
 
   it('do nothing on invalid change', async () => {
     installSubmissionCountSpy(SUBMISSIONS_PATH, LOI_ID, 1);
-    const loiUpdateSpy = installLoiUpdateSpy(LOI_PATH);
 
     await test.wrap(functions.onWriteSubmission)(
       {before: undefined, after: undefined},
       CONTEXT
     );
 
-    expect(loiUpdateSpy).not.toHaveBeenCalled();
+    const loi = await mockFirestore.doc(LOI_PATH).get();
+    expect(loi.data()).not.toHaveBeenCalled();
   });
 
-  it('throw error on failed update', async () => {
-    installSubmissionCountSpy(SUBMISSIONS_PATH, LOI_ID, 1);
-    const loiUpdateSpy = installLoiUpdateSpy(LOI_PATH);
-    loiUpdateSpy.and.throwError('LOI update failed');
+  // it('throw error on failed update', async () => {
+  //   installSubmissionCountSpy(SUBMISSIONS_PATH, LOI_ID, 1);
+  //   loi.data().and.throwError('LOI update failed');
 
-    await expectAsync(
-      test.wrap(functions.onWriteSubmission)(
-        {before: undefined, after: SUBMISSION},
-        CONTEXT
-      )
-    ).toBeRejected();
-  });
+  //   await expectAsync(
+  //     test.wrap(functions.onWriteSubmission)(
+  //       {before: undefined, after: SUBMISSION},
+  //       CONTEXT
+  //     )
+  //   ).toBeRejected();
+  // });
 });
