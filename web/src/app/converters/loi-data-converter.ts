8072b7ba9345238782efc50bc4ff4c3e5126ad4d
Gino Miceli <gmiceli@google.com>
2024-07-01 09:35:30 +0200

    Fix lint errors

--- web/src/app/converters/loi-data-converter.ts
+++ web/src/app/converters/loi-data-converter.ts
@@ -17,9 +17,11 @@ import {DocumentData} from '@angular/fire/firestore';
 import {toMessage} from '@ground/lib';
 import {GroundProtos} from '@ground/proto';
 import {Map} from 'immutable';
+
 import {toGeometry} from 'app/converters/geometry-converter';
 import {Geometry} from 'app/models/geometry/geometry';
 import {LocationOfInterest} from 'app/models/loi.model';
+
 import {geometryPbToModel} from './geometry-data-converter';
 
 import Pb = GroundProtos.google.ground.v1beta1;
