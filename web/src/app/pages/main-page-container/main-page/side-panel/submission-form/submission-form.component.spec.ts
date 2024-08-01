96d9f5f1ba274a03b4f9826774c1ae3fc6ac8f61
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-08-01 20:09:42 +0200

    linted

--- web/src/app/pages/main-page-container/main-page/side-panel/submission-form/submission-form.component.spec.ts
+++ web/src/app/pages/main-page-container/main-page/side-panel/submission-form/submission-form.component.spec.ts
@@ -27,6 +27,9 @@ import {MatRadioModule} from '@angular/material/radio';
 import {By} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {Router} from '@angular/router';
+import {List, Map} from 'immutable';
+import {NEVER, of} from 'rxjs';
+
 import {JobListItemModule} from 'app/components/job-list-item/job-list-item.module';
 import {AuditInfo} from 'app/models/audit-info.model';
 import {Coordinate} from 'app/models/geometry/coordinate';
@@ -49,8 +52,6 @@ import {LocationOfInterestService} from 'app/services/loi/loi.service';
 import {NavigationService} from 'app/services/navigation/navigation.service';
 import {SubmissionService} from 'app/services/submission/submission.service';
 import {SurveyService} from 'app/services/survey/survey.service';
-import {List, Map} from 'immutable';
-import {NEVER, of} from 'rxjs';
 
 import {SubmissionFormComponent} from './submission-form.component';
 
