21476a466a7a8ab334a6c4328c32a759f76f07ff
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-08-30 17:53:16 +0200

    removed LegacySubmissionDataConverter

--- web/src/app/converters/submission-data-converter.spec.ts
+++ web/src/app/converters/submission-data-converter.spec.ts
@@ -14,27 +14,63 @@
  * limitations under the License.
  */
 
-import {Timestamp} from '@angular/fire/firestore';
+import {registry} from '@ground/lib';
+import {GroundProtos} from '@ground/proto';
 import {Map} from 'immutable';
 
 import {Job} from 'app/models/job.model';
 import {Submission} from 'app/models/submission/submission.model';
 
 import {FirebaseDataConverter} from './firebase-data-converter';
-import {LegacySubmissionDataConverter} from './submission-data-converter';
+import {submissionDocToModel} from './submission-data-converter';
+
+import Pb = GroundProtos.ground.v1beta1;
+
+import {Task, TaskType} from 'app/models/task/task.model';
+
+const sb = registry.getFieldIds(Pb.Submission);
+const ai = registry.getFieldIds(Pb.AuditInfo);
+const td = registry.getFieldIds(Pb.TaskData);
+const td_tr = registry.getFieldIds(Pb.TaskData.TextResponse);
 
 const job001: Job = new Job(
   /* id= */ 'job001',
   /* index= */ 0,
   '#ffffff',
   'Test job',
-  Map({})
+  Map([['task001', new Task('task001', TaskType.TEXT, 'text', true, 1)]])
 );
+
+const submissionDoc001 = {
+  [sb.created]: {
+    [ai.userId]: 'creator001',
+    [ai.clientTimestamp]: undefined,
+    [ai.serverTimestamp]: undefined,
+    [ai.displayName]: 'Creator',
+    [ai.emailAddress]: 'creator@test.com',
+  },
+  [sb.lastModified]: {
+    [ai.userId]: 'modifier001',
+    [ai.clientTimestamp]: undefined,
+    [ai.serverTimestamp]: undefined,
+    [ai.displayName]: 'Modifier',
+    [ai.emailAddress]: 'modifier@test.com',
+  },
+  [sb.loiId]: 'loi001',
+  [sb.jobId]: 'job001',
+  [sb.taskData]: [
+    {[td.taskId]: 'task001', [td.textResponse]: {[td_tr.text]: 'text result'}},
+    // {[td.taskId]: 'task002', [td.numberResponse]: 123},
+    // {[td.taskId]: 'task003', 1: ['option001', 'option002']},
+    // {[td.taskId]: 'task004', 1: new Timestamp(1641533340, 0)},
+    // {[td.taskId]: 'task005', 1: new Timestamp(1641534444, 0)},
+  ],
+};
 
 const submission001 = {
   created: {
-    clientTimestamp: undefined,
-    serverTimestamp: undefined,
+    clientTimestamp: new Date(0),
+    serverTimestamp: new Date(0),
     user: {
       displayName: 'Creator',
       email: 'creator@test.com',
@@ -42,8 +78,8 @@ const submission001 = {
     },
   },
   lastModified: {
-    clientTimestamp: undefined,
-    serverTimestamp: undefined,
+    clientTimestamp: new Date(0),
+    serverTimestamp: new Date(0),
     user: {
       displayName: 'Modifier',
       email: 'modifier@test.com',
@@ -54,23 +90,23 @@ const submission001 = {
   jobId: 'job001',
   data: {
     task001: 'text result',
-    task002: ['option001', 'option002'],
-    task003: 123,
-    task004: new Timestamp(1641533340, 0),
-    task005: new Timestamp(1641534444, 0),
+    // task002: 123,
+    // task003: ['option001', 'option002'],
+    // task004: new Timestamp(1641533340, 0),
+    // task005: new Timestamp(1641534444, 0),
   },
 };
 
 describe('toSubmission', () => {
   it('Submission converts back and forth without losing data', () => {
-    expect(
-      FirebaseDataConverter.submissionToJS(
-        LegacySubmissionDataConverter.toSubmission(
-          job001,
-          'submission001',
-          submission001
-        ) as Submission
-      )
-    ).toEqual(submission001);
+    const submission001JS = FirebaseDataConverter.submissionToJS(
+      submissionDocToModel(
+        job001,
+        'submission001',
+        submissionDoc001
+      ) as Submission
+    );
+
+    expect(submission001JS).toEqual(submission001);
   });
 });
