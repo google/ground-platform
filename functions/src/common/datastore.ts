e1b4ea65e60e2d330c0f94a30911d714c4c277d1
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-09-30 14:26:53 +0200

    Fixes wrong WKT conversion at LOI creation time + Moved code into lib (#2057)

--- functions/src/common/datastore.ts
+++ functions/src/common/datastore.ts
@@ -179,10 +179,10 @@ export class Datastore {
   async updateLoiProperties(
     surveyId: string,
     loiId: string,
-    properties: {[key: string]: string}
+    loiDoc: DocumentData
   ) {
     const loiRef = this.db_.doc(loi(surveyId, loiId));
-    await loiRef.update({properties});
+    await loiRef.update({[l.properties]: loiDoc[l.properties]});
   }
 
   static toFirestoreMap(geometry: any) {
