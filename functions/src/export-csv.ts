9394e5715875363730f27a6e154f737fd129bacc
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-11-25 16:19:24 +0100

    fixed differences between empty Other and valued Other

--- functions/src/export-csv.ts
+++ functions/src/export-csv.ts
@@ -296,7 +296,9 @@ function getMultipleChoiceValues(
   if (values.length === 0 && !responses.otherText) values.push('Other');
   if (responses.otherText)
     values.push(
-      responses.otherText.trim() !== '' ? responses.otherText : 'Other'
+      responses.otherText.trim() !== ''
+        ? `Other: ${responses.otherText}`
+        : 'Other'
     );
   return values.join(',');
 }
