aa495bfaf0ab2a790e0348d1ca7bdd853d8346dc
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2024-12-09 14:02:05 +0100

    removed servers sub-collection in favour of servers array, fixed sender issue

--- functions/src/common/mail-service.ts
+++ functions/src/common/mail-service.ts
@@ -3,10 +3,11 @@ import {Datastore} from './datastore';
 
 type MailConfig = {
   defaultServer?: string;
-  servers?: {[key: string]: MailServerConfig};
+  servers?: MailServerConfig[];
 };
 
 type MailServerConfig = {
+  id: string;
   host: string;
   port: number;
   username: string;
@@ -21,33 +22,39 @@ export interface MailServiceEmail {
 }
 
 export class MailService {
-  private transporter_: any;
+  private transporter_: nodemailer.Transporter;
+  private sender_: string;
 
   constructor(mailServerConfig: MailServerConfig) {
     const {host, port, username, password, sender} = mailServerConfig;
+
+    this.sender_ = sender || username;
 
     this.transporter_ = nodemailer.createTransport({
       host,
       port,
       auth: {user: username, pass: password},
-      sender: sender || username,
+      sender: this.sender_,
     });
   }
 
   async sendMail(email: MailServiceEmail) {
     await new Promise<void>((resolve, reject) => {
-      this.transporter_.sendMail(email, (error: Error | any, _info: any) => {
-        if (error) {
-          // 501 and 550 are errors from the mail server: email address not found
-          if (error.responseCode === 501 || error.responseCode === 550) {
-            reject(new Error(error.response));
+      this.transporter_.sendMail(
+        {from: this.sender_, ...email},
+        (error: Error | any, _info: any) => {
+          if (error) {
+            // 501 and 550 are errors from the mail server: email address not found
+            if (error.responseCode === 501 || error.responseCode === 550) {
+              reject(new Error(error.response));
+            } else {
+              reject(error);
+            }
           } else {
-            reject(error);
+            resolve();
           }
-        } else {
-          resolve();
         }
-      });
+      );
     });
   }
 
@@ -57,8 +64,9 @@ export class MailService {
     const mailConfig = mail.data() as MailConfig;
     if (!mailConfig.defaultServer)
       throw new Error('Unable to find Default Server');
-    const mailServerConfig =
-      mailConfig.servers && mailConfig.servers[mailConfig.defaultServer];
+    const mailServerConfig = mailConfig.servers?.find(
+      ({id}) => id === mailConfig.defaultServer
+    );
     if (!mailServerConfig)
       throw new Error('Unable to find Mail Server Configuration');
     return mailServerConfig;
