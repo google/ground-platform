f1b8328e4956f5a87cd79a63e54899b3be39d008
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-02-19 18:48:33 +0100

    dependencies fix

--- web/src/main.ts
+++ web/src/main.ts
@@ -16,8 +16,6 @@
  * limitations under the License.
  */
 
-import 'hammerjs';
-
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
