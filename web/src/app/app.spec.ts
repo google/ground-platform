240abef6aa4185ca73a23538acde47fa9acc9f1c
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2025-06-20 14:27:39 +0200

    Switch from ngx-translate to @angular/localize for i18n (#2218)

--- web/src/app/app.spec.ts
+++ web/src/app/app.spec.ts
@@ -16,16 +16,14 @@
 
 import {TestBed, waitForAsync} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
-import {TranslateModule, TranslateService} from '@ngx-translate/core';
 
 import {AppComponent} from 'app/app.component';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, TranslateModule.forRoot()],
+      imports: [RouterTestingModule],
       declarations: [AppComponent],
-      providers: [TranslateService],
     }).compileComponents();
   }));
 
