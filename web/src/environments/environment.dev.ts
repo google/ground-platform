686350e94008873a837f181fe78d04452582de29
Gino Miceli <gmiceli@google.com>
2025-12-18 11:05:56 -0500

    Add hard-coded Firebase keys

--- web/src/environments/environment.dev.ts
+++ web/src/environments/environment.dev.ts
@@ -14,8 +14,18 @@
  * limitations under the License.
  */
 
-import { firebaseConfig } from 'environments/.firebase-config';
 import { Env } from 'environments/environment-enums';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDFMmOycj7L1Mqw_KYfn_IlFt4KgmYuO6k",
+  authDomain: "ground-dev-sig.firebaseapp.com",
+  projectId: "ground-dev-sig",
+  storageBucket: "ground-dev-sig.appspot.com",
+  messagingSenderId: "429183886479",
+  appId: "1:429183886479:web:96481f53f1202bc2293cfa",
+  measurementId: "G-7CZYYG7L4D"
+};
 
 export const environment = {
   production: false,
