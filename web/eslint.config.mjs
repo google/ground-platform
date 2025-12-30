ccfee9c20510e762a0f990c71ad5872f1b3b842e
Gino Miceli <gmiceli@google.com>
2025-12-30 17:51:31 -0500

    Fix lint failures

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -14,18 +14,17 @@
  * limitations under the License.
  */
 
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
+import gts from 'gts/build/eslint.config.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import angularTemplate from '@angular-eslint/template-parser';
+import angularTemplatePlugin from '@angular-eslint/eslint-plugin-template';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import typescriptParser from '@typescript-eslint/parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-});
 
 export default [
     {
@@ -39,17 +38,31 @@ export default [
             "**/src/generated"
         ],
     },
-    ...compat.config({
-        extends: [
-            "../node_modules/gts"
-        ],
-        env: {
-            jasmine: true,
-            browser: true,
-            es2022: true
+    ...gts,
+    {
+        // Global settings
+        languageOptions: {
+            globals: {
+                jasmine: "readonly",
+                describe: "readonly",
+                it: "readonly",
+                expect: "readonly",
+                beforeEach: "readonly",
+                afterEach: "readonly",
+                beforeAll: "readonly",
+                afterAll: "readonly",
+                spyOn: "readonly",
+                fail: "readonly",
+                browser: "readonly",
+                document: "readonly",
+                window: "readonly",
+                setTimeout: "readonly",
+                setInterval: "readonly",
+                clearTimeout: "readonly",
+                clearInterval: "readonly",
+                console: "readonly"
+            }
         },
-        parser: "@typescript-eslint/parser",
-        plugins: ["@typescript-eslint"],
         rules: {
             "eqeqeq": ["warn", "always", { "null": "ignore" }],
             "object-curly-spacing": ["warn", "always"],
@@ -69,48 +82,59 @@ export default [
             "n/no-deprecated-api": "off",
             "n/no-unsupported-features/es-builtins": "off",
             "n/no-unsupported-features/es-syntax": "off",
-            "n/no-unsupported-features/node-builtins": "off"
-        },
-        overrides: [
-            {
-                files: ["*.ts", "*.js"],
-                extends: [
-                    "plugin:@typescript-eslint/recommended",
-                    "plugin:prettier/recommended"
-                ],
-                rules: {
-                    "eqeqeq": ["warn", "always", { "null": "ignore" }],
-                    "prettier/prettier": "warn",
-                    "@typescript-eslint/no-unsafe-function-type": "off",
-                    "@typescript-eslint/no-empty-object-type": "off",
-                    "@typescript-eslint/no-wrapper-object-types": "off",
-                    "@typescript-eslint/no-explicit-any": "warn",
-                    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-                    "@typescript-eslint/no-unused-expressions": "warn",
-                    "@typescript-eslint/ban-ts-comment": "off",
-                    "@typescript-eslint/triple-slash-reference": "off",
-                    "no-var": "off",
-                    "prefer-const": "off",
-                    "no-empty-function": "off",
-                    "no-shadow": "off",
-                    "no-duplicate-imports": "off",
-                    "no-redeclare": "off"
-                }
-            },
-            {
-                files: ["*.html"],
-                parser: "@angular-eslint/template-parser",
-                extends: [
-                    "plugin:@angular-eslint/template/recommended",
-                    "plugin:prettier/recommended"
-                ],
-                rules: {
-                    "prettier/prettier": ["warn", { "parser": "angular" }],
-                    "@angular-eslint/template/prefer-control-flow": "off",
-                    "@angular-eslint/template/eqeqeq": "warn",
-                    "@angular-eslint/template/no-negated-async": "warn"
-                }
+            "n/no-unsupported-features/node-builtins": "off",
+            "prettier/prettier": "warn"
+        }
+    },
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: typescriptParser,
+            parserOptions: {
+                project: ["tsconfig.json"],
+                tsconfigRootDir: __dirname,
+                sourceType: "module"
             }
-        ]
-    })
+        },
+        plugins: {
+            "@typescript-eslint": typescriptEslintPlugin,
+            "prettier": prettierPlugin
+        },
+        rules: {
+            "eqeqeq": ["warn", "always", { "null": "ignore" }],
+            "prettier/prettier": "warn",
+            "@typescript-eslint/no-unsafe-function-type": "off",
+            "@typescript-eslint/no-empty-object-type": "off",
+            "@typescript-eslint/no-wrapper-object-types": "off",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+            "@typescript-eslint/no-unused-expressions": "warn",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "@typescript-eslint/triple-slash-reference": "off",
+            "no-var": "off",
+            "prefer-const": "off",
+            "no-empty-function": "off",
+            "no-shadow": "off",
+            "no-duplicate-imports": "off",
+            "no-redeclare": "off",
+            // Requested disabled rules
+            "@typescript-eslint/no-floating-promises": "off"
+        }
+    },
+    {
+        files: ["**/*.html"],
+        languageOptions: {
+            parser: angularTemplate,
+        },
+        plugins: {
+            "@angular-eslint/template": angularTemplatePlugin,
+            "prettier": prettierPlugin
+        },
+        rules: {
+            "prettier/prettier": ["warn", { "parser": "angular" }],
+            "@angular-eslint/template/prefer-control-flow": "off",
+            "@angular-eslint/template/eqeqeq": "warn",
+            "@angular-eslint/template/no-negated-async": "warn"
+        }
+    }
 ];
