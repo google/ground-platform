ee3331f22b3fa703ac2f2a1a10bc8a26148916de
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2026-04-01 01:39:08 +0200

    Extract property generator handlers and add geoid stub (#2478)

---
+++ functions/src/property-generators/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2026 The Ground Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export type {
+  Body,
+  Headers,
+  Properties,
+  PropertyGeneratorConfig,
+  PropertyGeneratorHandler,
+} from './types';
+
+import { geoidHandler } from './geoid';
+import { whispHandler } from './whisp';
+import type { PropertyGeneratorHandler } from './types';
+
+export const propertyGeneratorHandlers: Record<
+  string,
+  PropertyGeneratorHandler
+> = {
+  geoid: geoidHandler,
+  whisp: whispHandler,
+};
